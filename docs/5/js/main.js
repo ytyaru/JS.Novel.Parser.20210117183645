ba21894f28a4ce6a7973564d614c7d6d6d19ae03
ytyaru <ytyaru@outlook.jp>
2021-01-18 15:43:38 +0900

    add Denden.js

---
+++ docs/5/js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', (event) => {
+    document.querySelector('#selector').addEventListener('input', (event)=>{
+        parse();
+    });
+    document.querySelector('#editor').addEventListener('input', (event)=>{
+        parse();
+    });
+    function getParser() { return eval(document.querySelector('#selector').value); }
+    function parse() {
+        const editor = document.querySelector('#editor');
+        const viewer = document.querySelector('#viewer');
+        const drawer = document.querySelector('#drawer');
+        viewer.value = getParser().parse(editor.value);
+        drawer.innerHTML = BreakLine.parse(Paragraph.parse(viewer.value));
+    }
+    document.querySelector('#selector').focus();
+    document.querySelector('#selector').options[0].selected = true;
+    parse();
+});
